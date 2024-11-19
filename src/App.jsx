d6b9a918ef70e9b0dcf5722b23f30c19cf3fb5f2
MVENKATESH0786 <mvenkatesh0786@gmail.com>
2024-11-19 12:15:52 +0530

    Initial COmmit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import GroupSelector from "./components/GroupSelector";
 import SortSelector from "./components/SortSelector";
 import { fetchTickets } from "./api";
 
+//need to remove
+
 const App = () => {
   const [tickets, setTickets] = useState([]);
   const [groupBy, setGroupBy] = useState("status");
